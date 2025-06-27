da74b2fa9c42ad85587c91bcc666c9cdb3758883
faceless <bulatov22m11@gmail.com>
2025-06-27 22:17:13 +0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket  
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-server_socket.bind(('localhost', 12345))  
+server_socket.bind(('localhost', 8080))  
 server_socket.listen(5)  
 print("Сервер запущен. Ожидание подключений...")  
 while True:  
