221dc5c69d0c11104a6933469896c0bc30bfea06
faceless <bulatov22m11@gmail.com>
2025-06-27 22:25:04 +0400

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket  
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-client_socket.connect(('localhost', 8080))  
+client_socket.connect(('localhost', 3030))  
 client_socket.send("Привет, сервер!".encode('utf-8'))  
 response = client_socket.recv(1024).decode('utf-8')  
 print(f"Ответ от сервера: {response}")  
